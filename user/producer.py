1320e29ca64b79bcb735a9413b04ecace2eec6c5
bifenbecker <b1fenbecker@mail.ru>
2022-03-22 15:08:37 +0300

    support branch

--- user/producer.py
+++ user/producer.py
@@ -1,17 +1,20 @@
-import os
-import pika
+# import os
+# import pika
+#
+# credentials = pika.PlainCredentials('admin', 'admin')
+#
+# params = pika.ConnectionParameters(os.environ.get('RABBITMQ_HOST', 'localhost'), credentials=credentials)
+#
+# connection = pika.BlockingConnection(params)
+#
+# channel = connection.channel()
+#
+#
+# def logger(body, key='logger'):
+#     msg = f'Service: Auth - {body}'
+#     channel.basic_publish(exchange='',
+#                           routing_key=key,
+#                           body=msg)
 
-credentials = pika.PlainCredentials('admin', 'admin')
-
-params = pika.ConnectionParameters(os.environ.get('RABBITMQ_HOST', 'localhost'), credentials=credentials)
-
-connection = pika.BlockingConnection(params)
-
-channel = connection.channel()
-
-
-def logger(body, key='logger'):
-    msg = f'Service: Auth - {body}'
-    channel.basic_publish(exchange='',
-                          routing_key=key,
-                          body=msg)
+def logger(msg):
+    print(msg)
